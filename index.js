dc7fed8812cc3373c7b43be91157a83ff6fa8696
Andrew Davis <abd3721@gmail.com>
2022-10-22 12:39:33 -0700

    Convert this into a clone of bilara-book-builder

---
+++ index.js
@@ -0,0 +1,79 @@
+import buildSutta from "./buildSutta.js";
+import { books } from "./books_leejp.js";
+import optionSettings from "./optionSettings.js";
+import buildTitlePage from "./buildTitlePage.js";
+
+const suttaTable = document.querySelector("#sutta-html");
+const body = document.querySelector("body");
+
+optionSettings();
+
+suttaTable.innerHTML = "";
+
+// MAKE BOOK BUTTON
+const makeBookButton = document.getElementById("make-the-book");
+makeBookButton.addEventListener("click", () => {
+  suttaTable.innerHTML = "";
+  const progressLable = document.getElementById("progress-label");
+  const progressBar = document.getElementById("progress-bar");
+  const progressOuter = document.getElementById("progress-outer");
+  progressLable.innerHTML = "Working<span class='blink'>…</span>";
+  progressBar.style.width = "0%";
+  progressOuter.style.border = "solid 1px rgb(67, 67, 67)";
+  setTimeout(() => {
+    makeTheBook();
+  }, 50);
+});
+
+// COPY BUTTON
+const copyButton = document.getElementById("copy");
+copyButton.addEventListener("click", () => {
+  navigator.clipboard.writeText(suttaTable.innerHTML);
+});
+
+// Toggle ToC BUTTON
+const tocButton = document.getElementById("toc");
+tocButton.addEventListener("click", () => {
+  body.classList.toggle("hide-content");
+});
+
+//
+function makeTheBook() {
+  const selection = document.getElementById("selection").value;
+  const [slug, translator] = selection.split("|");
+  const bookContents = books[slug];
+  localStorage.completionCounter = 0;
+  var start = new Date();
+  buildTitlePage(slug, translator);
+
+  bookContents.forEach(article => {
+    if (/chapter/.test(article) || /section/.test(article) || /condensation/.test(article)) {
+      // this detects when there is a chapter title page that must be created
+      let [id, paliTitle, englishTitle] = article.split("|");
+      suttaTable.innerHTML += `<article id="${id}">
+    <div class="title">
+      <h1 title="${englishTitle}" class="${id.replace(
+        /\d+/,
+        ""
+      )}"><span class="pli-lang">${paliTitle}</span> <span class="eng-lang">${englishTitle}</span></h1>
+  </div>
+    </article>\n`;
+      localStorage.completionCounter++;
+      const progressBar = document.getElementById("progress-bar");
+      const width = (localStorage.completionCounter / bookContents.length) * 100;
+      progressBar.style.width = width + "%";
+
+      if (localStorage.completionCounter === bookContents.length) {
+        const progressLable = document.getElementById("progress-label");
+        progressLable.innerHTML = "Finished";
+        var finish = new Date();
+        console.log(finish - start);
+      }
+    } else {
+      // this adds the article tag into the dom for each sutta.
+      suttaTable.innerHTML += `<article id="${article}"></article>\n`;
+      // This starts the process of building the sutta that will be put into that above dom item when the process is finished.
+      buildSutta(`${slug}/${article}`, article, translator, bookContents.length, start);
+    }
+  });
+}
